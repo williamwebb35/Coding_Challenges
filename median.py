c8ff1de67f1cd34fdf9f5a14633588e9e11e8a7c
William C. Webb <williamwebb35@gmail.com>
2019-01-03 12:01:52 -0800

    Update median.py
    Included the version that prompts for user input

--- median.py
+++ median.py
@@ -15,3 +15,28 @@ def median(numbers):
     return(numbers[odd_med])
 
 median([4, 5, 5, 4])
+
+# Below is the version allow for user input, a single number at a time
+
+
+num_list =[]
+length = int(input("Please enter the number of numbers to consider:  "))
+for i in range(length):
+    number = input("Enter a number: ")
+    #While number:
+     #   number = input("Enter a number or 'N' to stop entering numbers: ")
+      #  number = int(new_num)
+    num_list.append(number)
+  
+numbers = sorted(num_list)
+amount = len(num_list)
+if amount % 2 == 0:
+    even_mid = int(amount/2)
+    even_mid_upper = int(num_list[even_mid])
+    even_mid_lower = int(num_list[even_mid -1])
+    even_med = ((even_mid_upper + even_mid_lower)/2)
+    print("The median is: ",even_med)
+else:
+    odd_med = int(amount/2)
+    print("The median is: ",(num_list[odd_med]))
+
