9df3ca744fb3c41f943eda2478b1d5b6bcb38b2a
William C. Webb <williamwebb35@gmail.com>
2019-02-01 08:16:00 -0800

    Add files via upload
    A very simple script that takes two integers and computes their sum. Completed for a coding challenge.

---
+++ SolveMeFirst.py
@@ -0,0 +1,10 @@
+#Complete the function solveMeFirst to compute the sum of two integers.
+
+solveMeFirst(1,2)
+
+def solveMeFirst():
+    num1 = int(input())
+    num2 = int(input())
+    print(num1 + num2)
+
+solveMeFirst()
